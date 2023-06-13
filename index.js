56db1316583df682ecfe7f4a48cdb099ad6f676e
BUSRA KUCUK <busra3590@gmail.com>
2023-06-13 23:21:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const visitedNumbers = [];
+  for (let num of array){
+    const difference = target - num;
+    if (visitedNumbers.includes(difference)){
+      return true;
+
+    }
+    visitedNumbers.push(num);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  The time complexity is linear, where n represents the number of elements
+  in the input array.
+  As the size of the array increases, the time taken by the function 
+  to complete its execution also increases proportionally.
 */
 
 /* 
   Add your pseudocode here
+  function hasTargetSum(array, target):
+  visitedNumbers = an empty array
+
+  for each num in array:
+
+  difference = target - num
+
+  if differenence is in visitedNumbers:
+  return true
+
+  add num to visitedNumbers
+
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  This function checks if any pairs of numbers in the array
+  can be added together to equal the target number.
+  the function calculates the difference between the target number and the
+  current number. if this difference has been visited before
+  it means a pair of numbers that adds up to the tarhet number
+  has been found, and the function returns true.
+  if the difference has not been visited before
+  the current number is added to the visited numbers list.
+  if no pair of numbers is found that adds up to the target number,
+  the function returns false.
 */
 
 // You can run `node index.js` to view these console logs
